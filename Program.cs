e236953646acbfecaa881c2a76f2fa24467a82f3
Kristian Gyulemetov <119662062+kris4aka@users.noreply.github.com>
2022-12-21 09:42:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                for (int k = 1; k < i; k++)
+                {
+                    Console.Write("$");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
